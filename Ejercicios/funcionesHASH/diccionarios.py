7e301abe894ed0bafcf8d85d2a4516591f0adf6f
jlopezsanchez <jf.lopezsanchezmon@edu.gva.es>
2024-03-15 12:01:48 +0100

    diccionarios

---
+++ Ejercicios/funcionesHASH/diccionarios.py
@@ -0,0 +1,14 @@
+direcciones = {
+    'juan': 'C/Moncofar, 5',
+    'antonio': 'C/Enmedio, 15',
+    'jose': 'C/Mayor, 30'
+}
+
+#print(direcciones.get('juanito'))
+for nombre, direccion in direcciones.items():
+    print(f'{nombre}: {direccion}')
+
+    
+
+
+
